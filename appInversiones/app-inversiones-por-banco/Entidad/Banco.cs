b3bfecf7f54e3043affe2503faf046ff2df59392
Joaquin Rodriguez <ertsyvler@gmail.com>
2026-07-02 19:13:25 -0300

    Estructura inicial del proyecto - division en capas / carpetas

---
+++ appInversiones/app-inversiones-por-banco/Entidad/Banco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_inversiones_por_banco.Entidad
+{
+    internal class Banco
+    {
+        private string nombre;
+        private decimal[] tasas = new decimal[3];
+
+        private decimal ObtenerPromedioTasas() {
+            decimal ejemplo = 0;
+
+            for (int i = 0; i < tasas.Length; i++)
+            {
+                ejemplo += tasas[i];
+            }
+
+            ejemplo /= tasas.Length;
+
+            return ejemplo;
+        }
+    }
+}
---
+++ appInversiones/app-inversiones-por-banco/Negocio/Calculos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_inversiones_por_banco.Negocio
+{
+    static public class Calculos
+    {
+        // Hacer funciones que devuelva el monto resultado de cada modalidad de inversion.
+
+        public static decimal CalcularInversionAnual(decimal monto, decimal tasa, int meses)
+        {
+            //decimal resultado = monto * (1 + (tasa / 100) * meses);
+            decimal resultado = 0;
+            return resultado;
+        }
+
+        public static decimal CalcularInversionTrimestral(decimal monto, decimal tasa, int meses)
+        {
+            // Recibir tasa dividir entre 4 y multiplicar por el monto y por la cantidad de trimestres (meses / 3)
+            decimal resultado = 0;
+            return resultado;
+        }
+
+        public static decimal CalcularInversionMensual(decimal monto, decimal tasa, int meses)
+        {
+            //decimal resultado = monto * (1 + (tasa / 100) * meses);
+            decimal resultado = 0;
+            return resultado;
+        }
+    }
+}
